5be838d4f7aa871621c7a9335bcadee717248773
Anmol Singh <anmolchandi48@gmail.com>
2025-09-21 00:12:33 +0530

    added dotenv package

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/tasks")
 const cors = require("cors")
-require("dotenv").config()
+require("dotenv").config({path: "../.env"})
 
 
 mongoose
